71399d728022976e62440213a4703ed50eac8902
yorikim <yorikim@gmail.com>
2015-12-02 12:40:31 +0600

    add lurker's documention

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :profiles, only: [:index] do
         get :me, on: :collection
